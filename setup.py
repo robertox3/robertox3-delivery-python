7c8d673f8c433bd7c355f7e35ee29cad87e3486a
romeuroberto <robertoromeu366@gmail.com>
2021-12-26 10:43:04 -0300

    Fix 4

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(
     packages= find_packages(),
     include_package_data=True,
     install_requires=[read("requirements.txt")],
-    extras_require={"dev": read("requirements-dev.txt") }
+    extras_require={"dev":read("requirements-dev.txt")}
 )
